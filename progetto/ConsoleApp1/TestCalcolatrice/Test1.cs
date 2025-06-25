d56e23af56d2d3efe477d424ce792c818cc08a4b
Matteo aka. Michael Pitolano <217023450+Caniz-27@users.noreply.github.com>
2025-06-25 15:43:28 +0200

    Aggiunta Calcolatrice e Test somma

---
+++ progetto/ConsoleApp1/TestCalcolatrice/Test1.cs
@@ -0,0 +1,21 @@
+﻿namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            CalcolatriceGitHub.Calcolatrice calcolatrice = new CalcolatriceGitHub.Calcolatrice();
+            double risultato = calcolatrice.Somma(2, 3);
+            Assert.AreEqual(5, risultato, 0.0001, "La somma di 2 e 3 dovrebbe essere 5.");
+        }
+        [TestMethod]
+        public void TestSommaNegativa()
+        {
+            CalcolatriceGitHub.Calcolatrice calcolatrice = new CalcolatriceGitHub.Calcolatrice();
+            double risultato = calcolatrice.Somma(-2, -3);
+            Assert.AreEqual(-5, risultato, 0.0001, "La somma di -2 e -3 dovrebbe essere -5.");
+        }
+    }
+}
